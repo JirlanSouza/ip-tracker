0c6d156f565f766ae9e0a387783091b972ca103a
Jirlan Souza <jirlansouza08@gmail.com>
2022-09-18 17:36:27 -0400

    Fix leaflet incomplete render error

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -8,15 +8,13 @@ const icon = new Icon({ iconUrl: iconSvg });
 export function Map() {
   const position: LatLngExpression = [51.505, -0.09];
   return (
-    <div className={styles.container}>
-      <MapContainer className={styles.map} center={position} zoom={12} scrollWheelZoom={false}>
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-        <Marker position={[position[0] + 0.17, position[1]]} icon={icon}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
-    </div>
+    <MapContainer className={styles.container} center={position} zoom={12} scrollWheelZoom={false}>
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <Marker position={position} icon={icon}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
 }
