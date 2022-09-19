0352770e9724fcb54c4a0d5a111fec33fc4c132e
Jirlan Souza <jirlansouza08@gmail.com>
2022-09-19 17:40:56 -0400

    Changed the IpInput component add props

--- src/components/IpInput/index.tsx
+++ src/components/IpInput/index.tsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
 import styles from "./styles.module.scss";
 
-export function IpInnput() {
+interface IpInputProps {
+  ipAdress: string;
+  setIpAdress: (ip: string) => void;
+}
+
+export function IpInnput({ ipAdress, setIpAdress }: IpInputProps) {
+  const [ipValue, setIpValue] = useState("");
+
+  function handleIpAdress() {
+    if (ipValue.length < 7) return;
+
+    setIpAdress(ipValue);
+  }
+
   return (
     <span className={styles.container}>
-      <input />
-      <button />
+      <input value={ipValue} onChange={(event) => setIpValue(event.target.value)} />
+      <button onClick={handleIpAdress} />
     </span>
   );
 }
