6a059664c130fea1413b95fe227f193ab9944d78
Jirlan Souza <jirlansouza08@gmail.com>
2022-09-19 17:39:32 -0400

    Changed the Home page add useGeoLocation hook

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,20 @@
 import { InfoCard } from "../../components/InfoCard";
 import { IpInnput } from "../../components/IpInput";
 import { Map } from "../../components/Map";
+import { useIpGeoLocation } from "../../hooks/geoLocation/useGeoLocation";
 import styles from "./styles.module.scss";
 
 export function Home() {
+  const { loading, error, ipAdress, ipGelocationInformation, setIpAdress } = useIpGeoLocation();
+
   return (
     <div className={styles.container}>
       <div className={styles.banner}>
         <h1>IP Adredd Tracker</h1>
-        <IpInnput />
+        <IpInnput ipAdress={ipAdress} setIpAdress={setIpAdress} />
         <InfoCard />
       </div>
-      <Map />
+      <Map ip={ipGelocationInformation?.ip} location={ipGelocationInformation?.location} />
     </div>
   );
 }
