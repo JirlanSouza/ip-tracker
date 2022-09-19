4026d63353467d08cd47b97074bb635b8c14e7bd
Jirlan Souza <jirlansouza08@gmail.com>
2022-09-19 17:38:09 -0400

    Changed the Map component extract Marker component

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,20 +1,26 @@
-import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
-import { Icon, LatLngExpression } from "leaflet";
+import { MapContainer, TileLayer } from "react-leaflet";
+import { LatLngExpression } from "leaflet";
 
+import { LocationMarker } from "../LocationMarker";
 import styles from "./styles.module.scss";
-import iconSvg from "../../assets/images/icon-location.svg";
-const icon = new Icon({ iconUrl: iconSvg });
 
-export function Map() {
-  const position: LatLngExpression = [51.505, -0.09];
+interface MapProps {
+  ip?: string;
+  location?: {
+    description: string;
+    lat: number;
+    lng: number;
+  };
+}
+
+export function Map({ ip, location }: MapProps) {
+  function getPosition(location?: { lat: number; lng: number }): LatLngExpression {
+    return [location?.lat ?? 0, location?.lng ?? 0];
+  }
   return (
-    <MapContainer className={styles.container} center={position} zoom={12} scrollWheelZoom={false}>
+    <MapContainer className={styles.container} center={getPosition(location)} zoom={12} scrollWheelZoom={false}>
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      <Marker position={position} icon={icon}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      <LocationMarker ip={ip} location={location} />
     </MapContainer>
   );
 }
