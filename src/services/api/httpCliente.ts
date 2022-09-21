7a74798074a0788a45b456e4e5ecadcdd0c34436
Jirlan Souza <jirlansouza08@gmail.com>
2022-09-20 21:34:10 -0400

    Added the JSON.parse, if response data is string

--- src/services/api/httpCliente.ts
+++ src/services/api/httpCliente.ts
@@ -4,12 +4,16 @@ import { IHttpClient } from "./IHttpCliente";
 export class HttpClient implements IHttpClient {
   private httpClient: Axios;
   constructor(baseUrl: string) {
-    this.httpClient = new Axios({ url: baseUrl });
+    this.httpClient = new Axios({ baseURL: baseUrl });
   }
 
   async get<T>(path: string): Promise<T | Error> {
     try {
       const response = await this.httpClient.get<T>(path);
+      if (typeof response.data === "string") {
+        response.data = JSON.parse(response.data);
+      }
+
       return response.data;
     } catch (err) {
       return err as Error;
