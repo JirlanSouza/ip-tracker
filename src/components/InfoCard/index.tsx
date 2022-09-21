a6a15440fc2695ddf3347713c4eeec3dcb0745b1
Jirlan Souza <jirlansouza08@gmail.com>
2022-09-20 20:12:58 -0400

    Added the responsive style in ui components

--- src/components/InfoCard/index.tsx
+++ src/components/InfoCard/index.tsx
@@ -3,22 +3,22 @@ import styles from "./styles.module.scss";
 export function InfoCard() {
   return (
     <div className={styles.container}>
-      <span>
+      <span className={styles.info}>
         <label>IP ADRESS</label>
         <p>192.212.174.101</p>
       </span>
 
-      <span>
+      <span className={styles.info}>
         <label>LOCATION</label>
         <p>Brooklyn, NY 10001</p>
       </span>
 
-      <span>
+      <span className={styles.info}>
         <label>TIMEZONE</label>
         <p>UTC -05:00</p>
       </span>
 
-      <span>
+      <span className={styles.info}>
         <label>ISP</label>
         <p>SpaceX Starlink</p>
       </span>
