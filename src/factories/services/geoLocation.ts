8f26b47b305b1900bab91e3572215d169c16b85e
Jirlan Souza <jirlansouza08@gmail.com>
2022-09-20 21:31:55 -0400

    Changed the env variables names

--- src/factories/services/geoLocation.ts
+++ src/factories/services/geoLocation.ts
@@ -1,10 +1,13 @@
 import { FakeIpGeoLocationApi } from "../../services/api/data/fakeIpGeoLocationApi";
 import { HttpClient } from "../../services/api/httpCliente";
-import { IpGelocationService } from "../../services/IpGeolocation";
+import { IpGeolocationService } from "../../services/IpGeolocation";
 
+console.log(import.meta.env);
 const httpClient = import.meta.env.PROD
-  ? new HttpClient(import.meta.env.GELOCATION_API_BASE_URL)
+  ? new HttpClient(import.meta.env.VITE_GEOLOCATION_API_BASE_URL)
   : new FakeIpGeoLocationApi();
-const IpGeoLocationService = new IpGelocationService(httpClient, import.meta.env.GET_GELOCATION_API_PATH);
+const ipGeoLocationService = new IpGeolocationService(httpClient, import.meta.env.VITE_GEOLOCATION_API_PATH);
 
-export { IpGeoLocationService };
+console.log(ipGeoLocationService);
+
+export { ipGeoLocationService };
